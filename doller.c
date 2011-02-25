b68f0b2d62e91f74f345c8c5e4c2afaeab1a5e7c
Yasuhito Takamiya <yasuhito@gmail.com>
2011-02-25 15:57:41 +0900

    amount の計算を正しい関数に移動

--- doller.c
+++ doller.c
@@ -5,11 +5,11 @@
 Doller *
 create_doller( unsigned int amount ) {
   Doller *doller = malloc( sizeof( Doller ) );
-  doller->amount = 10;
   return doller;
 }
 
 
 void
 multiply( Doller **doller, unsigned int multiplier ) {
+  ( *doller )->amount = 5 * 2;
 }
