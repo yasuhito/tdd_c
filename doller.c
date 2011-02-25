b62ac038cb96f345d9569868b64a29b56e34070d
Yasuhito Takamiya <yasuhito@gmail.com>
2011-02-25 16:02:32 +0900

    即値 5 を amount で置き換え

--- doller.c
+++ doller.c
@@ -5,11 +5,12 @@
 Doller *
 create_doller( unsigned int amount ) {
   Doller *doller = malloc( sizeof( Doller ) );
+  doller->amount = amount;
   return doller;
 }
 
 
 void
 multiply( Doller **doller, unsigned int multiplier ) {
-  ( *doller )->amount = 5 * 2;
+  ( *doller )->amount = ( *doller )->amount * 2;
 }
