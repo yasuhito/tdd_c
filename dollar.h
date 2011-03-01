4692b0b57a0f3060f52e34d53ff983dd3d5265ff
s-zenke <s-zenke@pb.jp.nec.com>
2011-02-28 18:17:47 -0800

    euqal を追加したよ

--- dollar.h
+++ dollar.h
@@ -4,4 +4,5 @@ typedef struct {
 
 
 Dollar *create_dollar( unsigned int amount );
+bool equal( const Dollar *dollar_1, const void *dollar_2 );
 Dollar *multiply( const Dollar *dollar, unsigned int multiplier );
