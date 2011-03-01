cfccb28bcb8bdfaf813dcdfca7321790faebb9c5
s-zenke <s-zenke@pb.jp.nec.com>
2011-03-01 15:46:30 -0800

    ソースに合わせて、equal の引数の名前を修正、include の追加

--- dollar.h
+++ dollar.h
@@ -1,8 +1,11 @@
+#include <stdbool.h>
+
+
 typedef struct {
   unsigned int amount;
 } Dollar;
 
 
 Dollar *create_dollar( unsigned int amount );
-bool equal( const Dollar *dollar_1, const void *dollar_2 );
+bool equal( const Dollar *dollar, const void *other );
 Dollar *multiply( const Dollar *dollar, unsigned int multiplier );
