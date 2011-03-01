af97c3161bcf5b37fa2a742da4162d4dc994b6a2
s-zenke <s-zenke@pb.jp.nec.com>
2011-02-28 18:03:43 -0800

    equal のテストを追加したよ

--- test_multiply.c
+++ test_multiply.c
@@ -21,10 +21,26 @@ test_multiply() {
 }
 
 
+static void
+test_equal() {
+  Doller *five_1 = create_doller( 5 );
+  Doller *five_2 = create_doller( 5 );
+  Doller *six = create_doller( 6 );
+
+  assert_true( equal( five_1, five_2 ) );
+  assert_false( equal( five_1, six ) );
+
+  free( five_1 );
+  free( five_2 );
+  free( six );
+}
+
+
 int
 main() {
   const UnitTest tests[] = {
     unit_test( test_multiply ),
+    unit_test( test_equal ),
   };
   return run_tests( tests );
 }
