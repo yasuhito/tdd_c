a87b734ec9f2e99363928427659c8e0b3d884152
n-tada <n-tada@zj.nis.jp.nec.com>
2011-03-03 02:19:20 -0800

    Dokkerをdollar.cに移動

--- dollar.h
+++ dollar.h
@@ -1,11 +1,6 @@
 #include <stdbool.h>
 
 
-typedef struct {
-  unsigned int amount;
-} Dollar;
-
-
 Dollar *create_dollar( unsigned int amount );
 bool equal( const Dollar *dollar, const void *other );
 Dollar *multiply( const Dollar *dollar, unsigned int multiplier );
