abe08e42444f37950b8e8ba4f164869d39185869
Yasuhito Takamiya <yasuhito@gmail.com>
2011-03-04 15:32:01 +0900

    test_multiply() が長いので関数を分けた

--- test_dollar.c
+++ test_dollar.c
@@ -7,34 +7,44 @@
 
 
 static void
-test_multiply() {
+test_multiply_5x2() {
   Dollar *five = create_dollar( 5 );
   Dollar *product = multiply( five, 2 );
   Dollar *ten = create_dollar( 10 );
+
   assert_true( equal( product, ten ) );
+
+  free( five );
   free( product );
   free( ten );
+}
 
-  product = multiply( five, 3 );
+
+static void
+test_multiply_5x3() {
+  Dollar *five = create_dollar( 5 );
+  Dollar *product = multiply( five, 3 );
   Dollar *fifteen = create_dollar( 15 );
+
   assert_true( equal( product, fifteen ) );
-  free( product );
+
   free( five );
+  free( product );
   free( fifteen );
 }
 
 
 static void
 test_equal() {
-  Dollar *five_1 = create_dollar( 5 );
-  Dollar *five_2 = create_dollar( 5 );
+  Dollar *five1 = create_dollar( 5 );
+  Dollar *five2 = create_dollar( 5 );
   Dollar *six = create_dollar( 6 );
 
-  assert_true( equal( five_1, five_2 ) );
-  assert_false( equal( five_1, six ) );
+  assert_true( equal( five1, five2 ) );
+  assert_false( equal( five1, six ) );
 
-  free( five_1 );
-  free( five_2 );
+  free( five1 );
+  free( five2 );
   free( six );
 }
 
@@ -42,7 +52,8 @@ test_equal() {
 int
 main() {
   const UnitTest tests[] = {
-    unit_test( test_multiply ),
+    unit_test( test_multiply_5x2 ),
+    unit_test( test_multiply_5x3 ),
     unit_test( test_equal ),
   };
   return run_tests( tests );
