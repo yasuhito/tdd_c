333b5d9ef4eaa2706406334c6b9e0ce1251bb25d
Yoshiaki Sakae <sakae@MacBook.local>
2011-05-22 17:52:04 +0900

    test_different_class_equality を削除

--- test_franc.c
+++ test_franc.c
@@ -34,19 +34,11 @@ test_multiply_franc_5x3() {
 }
 
 
-static void 
-test_different_class_equality() {
-  Money *tenmoney = create_money(10, CHF);
-  Franc *tenfranc = create_franc(10, CHF);
-  assert_true( equal( tenmoney, tenfranc ) );
-}
-
 int
 main() {
   const UnitTest tests[] = {
     unit_test( test_multiply_franc_5x2 ),
     unit_test( test_multiply_franc_5x3 ),
-    unit_test( test_different_class_equality)
   };
   return run_tests( tests );
 }
