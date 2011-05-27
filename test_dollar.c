a784369e75113d07199144afa56c1e12fc2cc772
Masumi Ichien <icenmsm@gmail.com>
2011-05-28 00:18:21 +0900

    加法の小さなテストをtest_dollar.cに追加

--- test_dollar.c
+++ test_dollar.c
@@ -52,12 +52,29 @@ test_equal() {
 }
 
 
+static void
+test_simple_addition() {
+  Money *five = dollar( 5 );
+  Money *addend_five = dollar( 5 );
+  Money *sum = plus( five, addend_five );
+  Money *ten = dollar( 10 );
+
+  assert_true( equal( ten, sum ) );
+
+  free( five );
+  free( addend_five );
+  free( sum );
+  free( ten );
+}
+
+
 int
 main() {
   const UnitTest tests[] = {
     unit_test( test_multiply_dollar_5x2 ),
     unit_test( test_multiply_dollar_5x3 ),
     unit_test( test_equal ),
+    unit_test( test_simple_addition ),
   };
   return run_tests( tests );
 }
