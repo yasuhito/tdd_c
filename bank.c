b9d26eba7cafc9c80f9ff396173a4c167dd9add1
Yasuhito Takamiya <yasuhito@gmail.com>
2011-06-26 22:07:41 +0900

    Fix code format

--- bank.c
+++ bank.c
@@ -1,9 +1,8 @@
 #include "bank.h"
 #include "money.h"
 
+
 Money *
 bank_reduce( Expression *source, Currency to ) {
-  return source->reduce(source, to);
+  return source->reduce( source, to );
 }
-
-
